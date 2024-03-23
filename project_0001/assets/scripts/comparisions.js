1ba125784a33cc30961f3b25ec7477d9a65b1455
Sharif MD. Jobayed -Ul- Islam <152864746+sharifjobayed-a1qa@users.noreply.github.com>
2024-03-23 16:26:44 +0000

    Unnecessary projects deleted

--- project_0001/assets/scripts/comparisions.js
+++
@@ -1,5 +0,0 @@
-
-
-console.log(5>8);
-console.log(23==24);
-console.log(434*23==100);
--- project_0001/assets/scripts/variables.js
+++
@@ -1,9 +0,0 @@
-
-
-var aVar="This is a var.";
-let aLet="This is a let.";
-const aConst="This is a const.";
-
-console.log(aVar);
-console.log(aLet);
-console.log(aConst);
