7487852e2a73ac08917d9ac4709ceb7807e19f04
s5.islam <s5.islam@a1qa.com>
2024-03-23 22:50:00 +0600

    Functions.js added

---
+++ assets/scripts/functions.js
@@ -0,0 +1,20 @@
+
+
+let num1=40;
+let num2=30;
+
+
+function one(){
+	console.log(num1+num2);
+}
+one();
+
+const two=()=>{
+	console.log(num1-num2);
+}
+two();
+
+const three=(a,b)=>{
+	console.log(a*b);
+}
+three(num1,num2);
