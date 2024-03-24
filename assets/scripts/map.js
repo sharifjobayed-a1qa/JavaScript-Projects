9bb9b7075616d9cab23d29b61cbf5bdb710caa77
s5.islam <s5.islam@a1qa.com>
2024-03-24 06:18:04 +0600

    map.js added

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -3,7 +3,8 @@
 let map1=new Map(
 	[
 		[`apple`,400],
-		[`orange`,200]
+		[`orange`,200],
+		[`berry`,500]
 	]
 );
 
@@ -14,5 +15,20 @@ console.log(map1.size);
 map1.delete(`Laptop`);
 console.log(map1.size);
 
-map1.clear();
-console.log(map1.size);
+/* map1.clear();
+console.log(map1.size); */
+
+const mapToObject=Object.fromEntries(map1);
+console.log(typeof(mapToObject));
+
+const objectToMap=new Map(Object.entries(mapToObject));
+console.log(objectToMap);
+
+// Iterate through the Map
+const mapIterate=()=>{
+	for(let i of map1){
+		console.log(map1[i]);
+		console.log(i);
+	}
+}
+mapIterate();
