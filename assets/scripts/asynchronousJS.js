3caad2c5a0c87e85c0b323c1618e70518e73a560
s5.islam <s5.islam@a1qa.com>
2024-03-25 13:07:37 +0600

    Another asyncronous functionsn added in asyncronousJS.js

--- assets/scripts/asynchronousJS.js
+++ assets/scripts/asynchronousJS.js
@@ -12,3 +12,17 @@ async function asyncJS1(aNum1,aNum2){
 }
 
 asyncJS1(23,90);
+
+
+const asyncJS2=async(aNum1,aNum2)=>{
+	try{
+		let result=await (aNum1+aNum2);
+		console.log(result);
+	}catch(exception){
+		new console.error(exception);
+	}finally{
+		console.log(`Operation ends.`);
+	}
+}
+
+asyncJS2(17,245);
